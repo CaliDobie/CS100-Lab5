5fbca0aefae7971f8c3f67ef1bdaa94394d940e4
CaliDobie <carel009@ucr.edu>
2022-02-09 08:42:46 -0800

    finished lab 5

--- op.hpp
+++ op.hpp
@@ -2,12 +2,23 @@
 #define __OP_HPP__
 
 #include "base.hpp"
+#include "sstream"
+#include <iomanip>
+#include <string>
 
 class Op : public Base {
+    private:
+	double value;
     public:
-        Op(double value) : Base() { }
-        virtual double evaluate() { return 0.0; }
-        virtual std::string stringify() { return ""; }
+        Op(double value) : Base() { this-> value = value; }
+        virtual double evaluate() { return value; }
+        virtual std::string stringify() { 
+			std::stringstream stream;
+			std::string valTxt;
+			stream << std::fixed << std:: setprecision(1) << value;
+			valTxt = stream.str();
+			return valTxt; 
+	}
 };
 
 #endif //__OP_HPP__
--- op_test.hpp
+++ op_test.hpp
@@ -10,4 +10,9 @@ TEST(OpTest, OpEvaluateNonZero) {
     EXPECT_EQ(test->evaluate(), 8);
 }
 
+TEST(OpTest, OpStringifyNonZero) {
+    Op* test = new Op(8);
+    EXPECT_EQ(test->stringify(), "8.0");
+}
+
 #endif //__OP_TEST_HPP__
